3cb8e061b17166bfdfbc43843ebc4422732778e3
dj_yoko <yokochan.recurrent+webdev@gmail.com>
2018-12-23 00:52:58 +0900

    eneble SCSS compile

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const {
 	VueLoaderPlugin
 } = require('vue-loader')
 
+const MODE = 'development';
+const enabledSourceMap = (MODE === 'development');
+
 module.exports = {
 	entry: __dirname + "/src/js/clocks.js",
 	output: {
@@ -17,6 +20,36 @@ module.exports = {
 			{
 				test: /\.js$/,
 				loader: 'babel-loader',
+      },
+			// Sassファイルの読み込みとコンパイル
+			{
+				test: /\.scss/, // 対象となるファイルの拡張子
+				use: [
+          // linkタグに出力する機能
+          'style-loader',
+          // CSSをバンドルするための機能
+					{
+						loader: 'css-loader',
+						options: {
+							// オプションでCSS内のurl()メソッドの取り込みを禁止する
+							url: false,
+							// ソースマップの利用有無
+							sourceMap: enabledSourceMap,
+
+							// 0 => no loaders (default);
+							// 1 => postcss-loader;
+							// 2 => postcss-loader, sass-loader
+							importLoaders: 2
+						},
+          },
+					{
+						loader: 'sass-loader',
+						options: {
+							// ソースマップの利用有無
+							sourceMap: enabledSourceMap,
+						}
+          }
+        ],
       },
 			{
 				test: /\.css$/,
