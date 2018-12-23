0e1c7799795cd83d9326130b290bd2c01a82918a
dj_yoko <yokochan.recurrent+webdev@gmail.com>
2018-12-23 17:14:15 +0900

    Store - add mutanion & action

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -7,5 +7,15 @@ export default new Vuex.Store({
 	state: {
 		region: 'ja',
 		datetime: new Date(),
+	},
+	mutations: {
+		updateTime: (state, payload) => {
+			state.datetime = payload;
+		}
+	},
+	actions: {
+		updateTime: (context, payload) => {
+			context.commit('updateTime', payload);
+		}
 	}
 });
