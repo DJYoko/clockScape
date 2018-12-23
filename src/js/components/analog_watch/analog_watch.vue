8d78afa940ffad3257c90c577befd962e3241c4e
dj_yoko <yokochan.recurrent+webdev@gmail.com>
2018-12-23 15:56:14 +0900

    add analog watch background markers

--- src/js/components/analog_watch/analog_watch.vue
+++ src/js/components/analog_watch/analog_watch.vue
@@ -1,10 +1,24 @@
 <template>
-	<p>{{datetime}}</p>
+	<div class="analog_watch">
+		<div class="background_display">
+			<watch-marker v-for="(item, index) in 30" :key="index" :index="index" class="watch_marker">
+			</watch-marker>
+		</div>
+		<div class="hands_container">
+			<div class="hand hour"></div>
+			<div class="hand minute"></div>
+			<div class="hand second"></div>
+		</div>
+	</div>
 </template>
 
 <script>
+	import watchMarker from './watch_marker';
 	export default {
 		name: 'analogWatch',
+		components: {
+			watchMarker,
+		},
 		props: {
 			datetime: {
 				type: String,
@@ -14,3 +28,28 @@
 	};
 
 </script>
+
+<style>
+	.analog_watch {
+		width: 300px;
+		height: 300px;
+		position: relative;
+	}
+
+	.hands_container,
+	.background_display {
+		width: 100%;
+		height: 100%;
+	}
+
+	.background_display {
+		position: relative;
+	}
+
+	.hands_container {
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
+
+</style>
---
+++ src/js/components/analog_watch/watch_marker.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="watch_marker" :style="markerStyle" :class="markerClass"></div>
+</template>
+
+<script>
+	export default {
+		name: 'watchMarker',
+		props: {
+			index: {
+				type: Number,
+				required: true,
+			}
+		},
+		computed: {
+			markerClass() {
+				return {
+					bold: this.index % 15 === 0,
+				}
+			},
+			markerStyle() {
+				return {
+					transform: 'rotateZ(' + this.index * 6 + 'deg)',
+				}
+			},
+		}
+	};
+
+</script>
+<style>
+	.watch_marker {
+		width: 100%;
+		height: 1px;
+		border: 2px solid #fff;
+		border-top: none;
+		border-bottom: none;
+		position: absolute;
+		top: 50%;
+		left: 0;
+	}
+
+	.bold {
+		border-width: 8px;
+	}
+
+</style>
