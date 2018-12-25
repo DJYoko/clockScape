2cb3d32e6a58d3c89c4320cbcf7d98724bbfae97
dj_yoko <yokochan.recurrent+webdev@gmail.com>
2018-12-25 19:17:28 +0900

    add Loading ServerTime function (server: NICT)

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -9,11 +9,26 @@ export default new Vuex.Store({
 		datetime: new Date(),
 	},
 	mutations: {
+		loadServerTime: (context) => {
+			$.ajax({
+				url: 'https://ntp-a1.nict.go.jp/cgi-bin/json',
+				method: 'GET',
+				success: function (data) {
+					console.log(data);
+				},
+				error: function (error) {
+					console.log(error);
+				}
+			});
+		},
 		updateTime: (state, payload) => {
 			state.datetime = payload;
 		}
 	},
 	actions: {
+		loadServerTime: (context) => {
+			context.commit('loadServerTime');
+		},
 		updateTime: (context, payload) => {
 			context.commit('updateTime', payload);
 		}
