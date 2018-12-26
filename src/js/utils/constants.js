befa59d9a59f96e6851283722191257718029b35
dj_yoko <yokochan.recurrent+webdev@gmail.com>
2018-12-27 01:29:05 +0900

    add webroot URI on Constants

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -14,18 +14,21 @@ const REGIONS = {
 		offset: -18000,
 		photoLink: 'https://unsplash.com/photos/UnI8AGe1634',
 	},
-		Shanghai: {
+	Shanghai: {
 		label: 'Shanghai',
 		offset: 28800,
 		photoLink: 'https://unsplash.com/photos/uKyzXEc2k_s',
 	},
-		Moscow: {
+	Moscow: {
 		label: 'Moscow',
 		offset: 10800,
 		photoLink: 'https://unsplash.com/photos/jaH3QF46gAY',
 	},
 }
 
+const WEB_ROOT = '/bitbucket/clockscape/dst/';
+
 export default {
 	REGIONS,
+	WEB_ROOT,
 }
