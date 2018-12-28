a5c4111da4832ec4fc4b9d0d85c874abdcdf55f0
dj_yoko <yokochan.recurrent+webdev@gmail.com>
2018-12-28 15:28:35 +0900

    add world map and credit of it.

--- src/js/components/index/_Index.vue
+++ src/js/components/index/_Index.vue
@@ -1,123 +1,134 @@
 <template>
-	<div id="view" :style="viewStyle">
-		<p class="text-right info-link">
-			<span class="glyphicon glyphicon-info-sign" data-toggle="modal" data-target="#info_modal"></span>
-		</p>
-		<div class="blackbox">
-			<div class="region_selector_wrapper">
-				<region-selector :region="region" @change="onRegionChange"></region-selector>
-			</div>
-			<clocks :unixtime="currentUnixtime" :region="region"></clocks>
-		</div>
-		<photo-info :region="region"></photo-info>
-	</div>
+    <div id="view" :style="viewStyle">
+        <p class="text-right info-link">
+            <span class="glyphicon glyphicon-info-sign" data-toggle="modal" data-target="#info_modal"></span>
+        </p>
+        <div class="blackbox">
+            <div class="region_selector_wrapper">
+                <region-selector :region="region" @change="onRegionChange"></region-selector>
+            </div>
+            <clocks :unixtime="currentUnixtime" :region="region"></clocks>
+        </div>
+        <div class="blackbox" id="map">
+            <img src="img/worldmap.svg">
+        </div>
+        <photo-info :region="region"></photo-info>
+    </div>
 </template>
 <script>
-	import CONSTANTS from '../../utils/constants';
-	import clocks from '../clocks/';
-	import regionSelector from '../regionSelector/';
-	import photoInfo from '../photoInfo/';
-	import {
-		mapState
-	} from 'vuex';
+    import CONSTANTS from '../../utils/constants';
+    import clocks from '../clocks/';
+    import regionSelector from '../regionSelector/';
+    import photoInfo from '../photoInfo/';
+    import {
+        mapState
+    } from 'vuex';
 
-	export default {
-		components: {
-			clocks,
-			regionSelector,
-			photoInfo,
-		},
-		data() {
-			return {
-				regions: CONSTANTS.REGIONS,
-			}
-		},
-		computed: {
-			...mapState([
-				'region',
-				'currentUnixtime',
-			]),
-			viewStyle() {
-				const style = {
-					padding: '10px',
-					backgroundImage: 'url(img/region/' + this.region + '.jpg)',
-				};
-				return style;
-			}
-		},
-		created() {
-			const payload = {};
-			this.syncRegion();
-			payload.callback = () => {
-				setInterval(() => {
-					this.$store.dispatch('updateTime');
-				}, 1000);
-			};
-			this.$store.dispatch('loadServerTime', payload);
-		},
-		updated() {
-			this.syncRegion();
-		},
-		methods: {
-			onRegionChange(payload) {
-				this.$store.dispatch('selectRegion', payload);
-			},
-			syncRegion() {
-				if (this.region === this.$route.params.region) {
-					return false;
-				}
-				const init_region = (this.$route.params.region) ? this.$route.params.region : 'Tokyo';
-				
-				this.$store.dispatch('selectRegion', {
-					region: init_region
-				});
-			},
-		},
-	};
+    export default {
+        components: {
+            clocks,
+            regionSelector,
+            photoInfo,
+        },
+        data() {
+            return {
+                regions: CONSTANTS.REGIONS,
+            }
+        },
+        computed: {
+            ...mapState([
+                'region',
+                'currentUnixtime',
+            ]),
+            viewStyle() {
+                const style = {
+                    padding: '10px',
+                    backgroundImage: 'url(img/region/' + this.region + '.jpg)',
+                };
+                return style;
+            }
+        },
+        created() {
+            const payload = {};
+            this.syncRegion();
+            payload.callback = () => {
+                setInterval(() => {
+                    this.$store.dispatch('updateTime');
+                }, 1000);
+            };
+            this.$store.dispatch('loadServerTime', payload);
+        },
+        updated() {
+            this.syncRegion();
+        },
+        methods: {
+            onRegionChange(payload) {
+                this.$store.dispatch('selectRegion', payload);
+            },
+            syncRegion() {
+                if (this.region === this.$route.params.region) {
+                    return false;
+                }
+                const init_region = (this.$route.params.region) ? this.$route.params.region : 'Tokyo';
+
+                this.$store.dispatch('selectRegion', {
+                    region: init_region
+                });
+            },
+        },
+    };
 
 </script>
 
 <style lang="scss">
-	#view {
-		background-color: #000;
-		height: 100%;
-		overflow: hidden;
-		text-align: center;
-		background-size: cover;
-		background-repeat: no-repeat;
-		background-position: center;
-	}
+    #view {
+        background-color: #000;
+        height: 100%;
+        overflow: hidden;
+        text-align: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+    }
 
-	.blackbox {
-		width: 200px;
-		margin: 0 0 0 auto;
-		padding: 20px 0;
-		background-color: rgba(0, 0, 0, 0.6);
-		border-radius: 4px;
-	}
+    .blackbox {
+        @media screen and (min-width: 768px) {
+            width: 200px;
+        }
+        margin: 0 0 20px auto;
+        padding: 20px 0;
+        background-color: rgba(0, 0, 0, 0.6);
+        border-radius: 4px;
+    }
 
-	.region_selector_wrapper {
-		text-align: right;
-		padding-right: 20px;
-		margin-bottom: 20px;
-	}
+    .region_selector_wrapper {
+        text-align: right;
+        padding-right: 20px;
+        margin-bottom: 20px;
+    }
 
+    .info-link {
 
+        .glyphicon {
+            cursor: pointer;
+            background: rgba(255, 255, 255, 0.5);
+            font-size: 16px;
+            padding: 4px 4px 2px;
+            border-radius: 16px;
+            color: #262626;
 
-	.info-link {
+            &:hover {
+                opacity: 0.75;
+            }
+        }
+    }
 
-		.glyphicon {
-			cursor: pointer;
-			background: rgba(255, 255, 255, 0.5);
-			font-size: 16px;
-			padding: 4px 4px 2px;
-			border-radius: 16px;
-			color: #262626;
-
-			&:hover {
-				opacity: 0.75;
-			}
-		}
-	}
+    #map {
+        padding: 0;
+        img {
+            display: block;
+            width: 100%;
+        }
+    }
 
 </style>
