f7e8a8c90df078f9bbf71a6c5ace5c0d8d074a4e
dj_yoko <yokochan.recurrent@gmail.com>
2019-07-18 00:59:20 +0900

    add build path resolve alias

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,67 @@
-const {
-	VueLoaderPlugin
-} = require('vue-loader')
+const { VueLoaderPlugin } = require('vue-loader')
 
-const MODE = 'development';
-const enabledSourceMap = (MODE === 'development');
+const MODE = 'development'
+const enabledSourceMap = MODE === 'development'
+const path = require('path')
 
 module.exports = {
-	entry: __dirname + "/src/js/clocks.js",
-	output: {
-		path: __dirname + '/docs/js/',
-		filename: 'clocks.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.vue$/,
-				loader: 'vue-loader'
+  entry: __dirname + '/src/js/clocks.js',
+  output: {
+    path: __dirname + '/docs/js/',
+    filename: 'clocks.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
       },
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
       },
-			{
-				test: /\.scss/,
-				use: [
-                    'style-loader',
-					{
-						loader: 'css-loader',
-						options: {
-							url: false,
-							sourceMap: enabledSourceMap,
-							// 0 => no loaders (default);
-							// 1 => postcss-loader;
-							// 2 => postcss-loader, sass-loader
-							importLoaders: 2
-						},
+      {
+        test: /\.scss/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              sourceMap: enabledSourceMap,
+              // 0 => no loaders (default);
+              // 1 => postcss-loader;
+              // 2 => postcss-loader, sass-loader
+              importLoaders: 2
+            }
           },
-					{
-						loader: 'sass-loader',
-						options: {
-							sourceMap: enabledSourceMap,
-						}
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: enabledSourceMap
+            }
           }
-        ],
+        ]
       },
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       },
-			{
-				test: /\.png$/,
-				loader: 'file-loader',
-				query: {
-					name: '[name].[ext]'
-				}
-    },
+      {
+        test: /\.png$/,
+        loader: 'file-loader',
+        query: {
+          name: '[name].[ext]'
+        }
+      }
     ]
-	},
-	resolve: {
-
-		extensions: ['.js', '.vue'],
-		alias: {
-
-			vue$: 'vue/dist/vue.esm.js',
-		},
-	},
-	plugins: [
-    new VueLoaderPlugin()
-  ],
-};
+  },
+  resolve: {
+    extensions: ['.js', '.vue'],
+    alias: {
+      vue$: 'vue/dist/vue.esm.js',
+      '@js': path.resolve(__dirname, 'src/js')
+    }
+  },
+  plugins: [new VueLoaderPlugin()]
+}
