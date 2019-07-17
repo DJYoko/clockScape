5f347c4fa978769bac514d78a77976df767d63c1
dj_yoko <yokochan.recurrent@gmail.com>
2019-07-18 01:17:07 +0900

    change City order to AtoZ of name

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -6,12 +6,12 @@ const REGIONS = {
     position: [33, 48.3],
     latlng: [51.5287718, -0.2416808]
   },
-  Tokyo: {
-    label: 'Tokyo',
-    offset: 32400,
-    photoLink: 'https://unsplash.com/photos/CudNrzbsyZw',
-    position: [42.7, 83.4],
-    latlng: [35.6693863, 139.6012968]
+  Moscow: {
+    label: 'Moscow',
+    offset: 10800,
+    photoLink: 'https://unsplash.com/photos/jaH3QF46gAY',
+    position: [32.5, 57.3],
+    latlng: [55.5815245, 36.8251335]
   },
   NewYork: {
     label: 'New York',
@@ -27,12 +27,12 @@ const REGIONS = {
     position: [45, 78.7],
     latlng: [31.2246325, 121.1965671]
   },
-  Moscow: {
-    label: 'Moscow',
-    offset: 10800,
-    photoLink: 'https://unsplash.com/photos/jaH3QF46gAY',
-    position: [32.5, 57.3],
-    latlng: [55.5815245, 36.8251335]
+  Tokyo: {
+    label: 'Tokyo',
+    offset: 32400,
+    photoLink: 'https://unsplash.com/photos/CudNrzbsyZw',
+    position: [42.7, 83.4],
+    latlng: [35.6693863, 139.6012968]
   }
 }
 
