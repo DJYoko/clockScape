f950168eb9a45c6c9513eefe2d09a0a7033088d4
DJYoko <yokochan.recurrent@gmail.com>
2020-01-09 22:33:06 +0900

    separate router from base JS

--- src/js/clocks.js
+++ src/js/clocks.js
@@ -2,21 +2,7 @@ import Vue from 'vue'
 import 'babel-polyfill'
 import Router from 'vue-router'
 import store from './store/'
-import Index from '@js/components//index'
-import CONSTANTS from '@js/utils/constants'
-
-const router = new Router({
-  mode: 'history',
-  base: '/',
-  routes:[
-    {
-      path: CONSTANTS.WEB_ROOT,
-      name: 'index',
-      component: Index,
-      alias: CONSTANTS.WEB_ROOT + 'index.html'
-    }
-  ]
-})
+import router from './router'
 
 window.addEventListener(
   'DOMContentLoaded',
