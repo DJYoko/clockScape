fab64468b737bbf4b51b9c9081f0090f894a5d65
DJYoko <yokochan.recurrent@gmail.com>
2020-01-09 10:24:51 +0900

    refactor Route behavior (not complete, all operation is describe in base js. route operations should be describe in route/index.js)

--- src/js/clocks.js
+++ src/js/clocks.js
@@ -1,11 +1,32 @@
-import 'babel-polyfill'
 import Vue from 'vue'
-import router from './router/'
+import 'babel-polyfill'
+import Router from 'vue-router'
 import store from './store/'
+import Index from '@js/components//index'
+import CONSTANTS from '@js/utils/constants'
 
-// eslint-disable-next-line
-new Vue({
-  el: '#wrapper',
-  router,
-  store
+const router = new Router({
+  mode: 'history',
+  base: '/',
+  routes:[
+    {
+      path: CONSTANTS.WEB_ROOT,
+      name: 'index',
+      component: Index,
+      alias: CONSTANTS.WEB_ROOT + 'index.html'
+    }
+  ]
 })
+
+window.addEventListener(
+  'DOMContentLoaded',
+  function() {
+    Vue.use(Router)
+
+    new Vue({
+      router,
+      store
+    }).$mount('#wrapper')
+  },
+  false
+)
