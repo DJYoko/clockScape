25a9bbc443061758689618d1a9af4ef4639926e6
DJYoko <yokochan.recurrent@gmail.com>
2020-05-02 21:44:56 +0900

    [Refactor] move Server-Connection from mutation to action

--- src/js/store/main/actions.js
+++ src/js/store/main/actions.js
@@ -1,6 +1,42 @@
+import constants from '@js/utils/constants'
+
 const actions = {
   loadServerTime ({ commit }, payload) {
-    commit('loadServerTime', payload)
+
+    const nd = new Date()
+    const initDeviceUnixTime = nd.getTime()
+
+    const promise = $.ajax({
+      url: constants.TIME_API_URL,
+      method: 'GET'
+    })
+
+    // conection success
+    promise.done(data => {
+      const initServerUnixtime = data.st * 1000
+      const payload = {
+        initDeviceUnixTime,
+        initServerUnixtime
+      }
+      commit('loadServerTime', payload)
+    })
+
+    // connection failed
+    promise.fail(error => {
+      const nd = new Date()
+      const initServerUnixtime = nd.getTime()
+      const payload = {
+        initDeviceUnixTime,
+        initServerUnixtime
+      }
+      commit('loadServerTime', payload)
+    })
+
+    // both of success & fail
+    promise.then(() => {
+      payload.callback()
+    })
+
   },
   updateTime ({ commit }) {
     commit('updateTime')
--- src/js/store/main/mutations.js
+++ src/js/store/main/mutations.js
@@ -1,26 +1,7 @@
-import constants from '@js/utils/constants'
-
 const mutations =  {
   loadServerTime: (state, payload) => {
-    const promise = $.ajax({
-      url: constants.TIME_API_URL,
-      method: 'GET'
-    })
-
-    promise.done(data => {
-      state.initServerUnixtime = data.st * 1000
-    })
-
-    promise.fail(error => {
-      const nd = new Date()
-      state.initServerUnixtime = nd.getTime()
-    })
-
-    promise.then(() => {
-      const nd = new Date()
-      state.initDeviceUnixTime = nd.getTime()
-      payload.callback()
-    })
+    state.initServerUnixtime = payload.initServerUnixtime
+    state.initDeviceUnixTime = payload.initDeviceUnixTime
   },
   updateTime: state => {
     const _nd = new Date()
